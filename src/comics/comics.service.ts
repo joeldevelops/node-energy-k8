810bfe79260a18a4c44ba6120803e6b5e3e68210
Joel Milligan <joelsaltwater@gmail.com>
2024-03-12 13:35:47 +0100

    Add github builds

--- src/comics/comics.service.ts
+++ src/comics/comics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Comic, Prisma } from '@prisma/client';
+import { Worker } from 'worker_threads';
 
 @Injectable()
 export class ComicsService {
@@ -58,11 +59,15 @@ export class ComicsService {
   // So here is an implementation of a Fibonacci sequence calculator with inefficiencies.
   // It will be called from the generate-report endpoint to simulate a big report.
   async calculateHighFibonacci(number: number): Promise<number> {
-    const fib = (n: number): number => {
-      if (n <= 1) return n;
-      return fib(n - 1) + fib(n - 2);
-    };
+    return new Promise((resolve, reject) => {
+      const worker = new Worker('./dist/comics/comics.worker.js');
+      worker.postMessage(number);
   
-    return fib(number);
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
   }
 }
